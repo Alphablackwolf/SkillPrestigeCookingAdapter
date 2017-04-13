cab7618a4a87382b74b3e9f277f30899afac04b8
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-04-13 00:47:57 -0400

    update compatibility with SMAPI 1.9+

--- SkillPrestigeCookingAdapter/SkillPrestigeCookingAdapterMod.cs
+++ SkillPrestigeCookingAdapter/SkillPrestigeCookingAdapterMod.cs
@@ -2,19 +2,33 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewValley;
-using SkillPrestige;
 
 namespace SkillPrestigeCookingAdapter
 {
     public class SkillPrestigeCookingAdapterMod : Mod
     {
+        /*********
+        ** Properties
+        *********/
+        private static IModHelper Helper;
         private static Texture2D _cookingSkillTexture;
         private const string IconFileName = @"iconA.png";
+
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            SkillPrestigeCookingAdapterMod.Helper = helper;
+        }
+
         public static Texture2D GetCookingSkillTexture()
         {
             if (_cookingSkillTexture != null) return _cookingSkillTexture;
@@ -35,43 +49,16 @@ namespace SkillPrestigeCookingAdapter
 
         private static string GetCookingSkillModPath()
         {
-            var type = typeof(Program);
-            var info = type.GetField("_modPaths", BindingFlags.NonPublic | BindingFlags.Static);
-            var modPaths = (List<string>)info.GetValue(null);
-            foreach (var modPath in modPaths)
-            {
-                foreach (var directory in Directory.GetDirectories(modPath))
-                {
-                    foreach (var file in Directory.GetFiles(directory, "manifest.json"))
-                    {
-                        if (file.Contains("StardewInjector")) continue;
-                        var baseConfig = new Manifest();
-                        Manifest manifest;
-                        try
-                        {
-                            if (string.IsNullOrEmpty(File.ReadAllText(file)))
-                            {
-                                continue;
-                            }
-                            manifest = baseConfig.InitializeConfig(file);
-                            if (string.IsNullOrEmpty(manifest.EntryDll))
-                            {
-                                continue;
-                            }
-                        }
-                        catch (Exception)
-                        {
-                            continue;
-                        }
-                        var directoryName = Path.GetDirectoryName(file);
-                        if (manifest.UniqueID == "CookingSkill")
-                        {
-                            return directoryName;
-                        }
-                    }
-                }
-            }
-            return string.Empty;
+            IModHelper modHelper = SkillPrestigeCookingAdapterMod.Helper;
+
+            // get Cooking Skill mod's implementation from SMAPI internals
+            // (This is a terrible idea, please don't do this.)
+            IMod cookingSkillMod = modHelper.Reflection
+                .GetPrivateValue<List<IMod>>(modHelper.ModRegistry, "Mods")
+                .FirstOrDefault(p => p.ModManifest.UniqueID == "CookingSkill");
+
+            // get mod's directory path
+            return cookingSkillMod?.Helper.DirectoryPath;
         }
     }
 }
